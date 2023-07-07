bd33361cc53c1814bdec6fbf51e4eb1af57903fb
moonkabir <moonkabir4@gmail.com>
2023-07-07 10:46:22 +0600

    template mastering done and login and register page done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,29 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+@extends('admin.authMaster')
+@section('title')
+    Login
+@endsection
+@section('authMainContent')
+    <h4>New here?</h4>
+    <h6 class="font-weight-light">Signing up is easy. It only takes a few steps</h6>
+    <form class="pt-3" method="POST" action="{{ route('register') }}">
+        @csrf
+        <div class="form-group">
+            <input type="text" class="form-control form-control-lg" id="exampleInputUsername1" placeholder="Username" name="name" required autofocus>
+        </div>
+        <div class="form-group">
+            <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email" name="email" required>
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password" name="password" required>
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control form-control-lg" id="password_confirmation" placeholder="password_confirmation" name="password_confirmation" required>
+        </div>
+        <div class="mt-3">
+            <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">SIGN UP</button>
+        </div>
+        <div class="text-center mt-4 font-weight-light">
+            Already have an account? <a href="{{ route('login') }}" class="text-primary">Login</a>
+        </div>
+    </form>
+@endsection
